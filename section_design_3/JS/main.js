c0107a260cb599bca0a18f79de87cec7dd3de4e5
rezashadhin <cat330winter@gmail.com>
2019-08-16 17:38:43 +0600

    section no 3

---
+++ section_design_3/JS/main.js
@@ -0,0 +1,11 @@
+(function ($){
+    jQuery(document).ready(function($){
+        $('.video_play_btn').magnificPopup({
+            type: 'video',
+        });
+})
+
+jQuery(window).load(function(){
+
+});
+}(jQuery));
